c2d6ac5810fce72d4319b48d5d7446fed9b4cbea
samsepi0l <52134317+muscleblazerahul@users.noreply.github.com>
2020-04-11 22:31:49 +0530

    Update subsetting-numpy-arrays.py

--- 01-intro-to-python-for-data-science/4-numpy/subsetting-numpy-arrays.py
+++ 01-intro-to-python-for-data-science/4-numpy/subsetting-numpy-arrays.py
@@ -20,16 +20,16 @@ Instructions
 index 110
 '''
 # height and weight are available as a regular lists
-
-# Import numpy
-import numpy as np
-
-# Store weight and height lists as numpy arrays
-np_weight = np.array(weight)
-np_height = np.array(height)
-
-# Print out the weight at index 50
-print(weight[50])
-
-# Print out sub-array of np_height: index 100 up to and including index 110
-print(np_height[100:111])
+    
+    # Import numpy
+    import numpy as np
+    
+    # Store weight and height lists as numpy arrays
+    np_weight_lb = np.array(weight_lb)
+    np_height_in = np.array(height_in)
+    
+    # Print out the weight at index 50
+    print(np_weight_lb[50])
+    
+    # Print out sub-array of np_height_in: index 100 up to and including index 110
+    print(np_height_in[100:111])
